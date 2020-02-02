88e706b72b2f59a9ed6e8db157be114f02dfea94
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-02-02 21:35:18 +0900

    UPDATE setup for nfs-server.

--- statefulsets-basic/setup.sh
+++ statefulsets-basic/setup.sh
@@ -1,13 +1,16 @@
 launch.sh
 
-apt-get install nfs-common
+apt-get install nfs-kernel-server
+mkdir /root/nfs-share
+echo "/root/nfs-share 172.17.0.29/16(rw,no_root_squash)" >> /etc/exports
+service nfs-kernel-server restart
 
 helm init
 
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 
 while true; do
-    NUM_READY=`helm install --set nfs.server=172.17.0.65 --set nfs.path=/root/nfs-share --name nfs-provisioner stable/nfs-client-provisioner | grep Error | wc -l`
+    NUM_READY=`helm install --set nfs.server=172.17.0.65 --set nfs.path=/root/nfs-share --name nfs-provisioner stable/nfs-client-provisioner | grep 'Error: could not find a ready tiller pod' | wc -l`
     if [ "${NUM_READY}" == "1" ]; then
         echo "Waiting for helm nfs server ready."
     else
